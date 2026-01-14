5a4b62b32753a7096091ea96e764adf14c17ff32
rishabhsingh1170 <rishabhsingh1170@gmail.com>
2026-01-14 16:23:55 +0530

    Fix urlextract import on Streamlit Cloud

--- helper.py
+++ helper.py
@@ -9,6 +9,7 @@ import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 nltk.download('stopwords')
 
 extractor = URLExtract()
